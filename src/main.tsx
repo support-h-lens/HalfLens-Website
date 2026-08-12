5e2acc188aa9d7ad45fd553b287a3785712104f2
ALSABAEEI <amro.alssapai@gmail.com>
2026-08-12 15:18:53 +0300

    Stable checkpoint before editorial refinement

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/ibm-plex-sans-arabic/arabic-300.css'
+import '@fontsource/ibm-plex-sans-arabic/arabic-400.css'
+import '@fontsource/ibm-plex-sans-arabic/arabic-500.css'
+import '@fontsource/manrope/latin-400.css'
+import '@fontsource/manrope/latin-500.css'
+import '@fontsource/manrope/latin-600.css'
 import App from './App'
 import './styles/global.css'
 
